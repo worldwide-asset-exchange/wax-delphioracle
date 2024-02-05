80dd38a1526dbbe824815cc85087711d981e1248
danielvo11 <quy.vok37@gmail.com>
2024-02-05 14:33:44 +0700

    replace bittrex by coinmarketcap and coinbase

--- src/config.ts
+++ src/config.ts
@@ -9,6 +9,25 @@ if (!process.env.PRIVATE_KEYS) throw new Error("PRIVATE_KEYS is required");
 if (!process.env.ACCOUNT) throw new Error("ACCOUNT is required");
 if (!process.env.PERMISSION) throw new Error("PERMISSION is required");
 
+export enum PriceSource {
+  coinbase = "coinbase",
+  coinmarketcap = "coinmarketcap"
+}
+
+export let PRICE_SOURCE = PriceSource.coinbase;
+export let PRICE_API_KEY = process.env.PRICE_API_KEY;
+
+if (process.env.PRICE_SOURCE) {
+  PRICE_SOURCE = PriceSource[process.env.PRICE_SOURCE];
+  if (!PRICE_SOURCE) {
+    throw new Error('Invalid price source');
+  }
+}
+
+if (PRICE_SOURCE === PriceSource.coinmarketcap && !PRICE_API_KEY) {
+  throw new Error('api key is required to use conmarketcap')
+}
+
 const signatureProvider = new JsSignatureProvider(process.env.PRIVATE_KEYS.split(","));
 export const rpc: any = new JsonRpc(process.env.NODEOS_ENDPOINT || '', { fetch: require('node-fetch') });
 export const api = new Api({ rpc, signatureProvider, textDecoder: new TextDecoder(), textEncoder: new TextEncoder() });
